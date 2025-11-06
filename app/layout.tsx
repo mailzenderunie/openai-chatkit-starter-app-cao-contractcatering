0648f932c8cf24e43f665b3b451e768cb2ccce6a
mailzenderunie <mailzenderunie@gmail.com>
2025-11-06 10:37:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bitter:wght@600&family=PT+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
+
         <Script
           src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
           strategy="beforeInteractive"
